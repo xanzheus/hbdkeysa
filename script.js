6acd46a096446e34da821fec0939fc92f23bb661
xənzheus <91020770+xanzheus@users.noreply.github.com>
2022-03-02 20:27:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+let nama, val;
+const url_string = document.URL;
+const url = new URL(url_string);
+let doi;
+
+if (url.searchParams.get('in') != null) {
+  doi = url.searchParams.get('in');
+} else {
+  doi = "Fadhli";
+}
+
 let footer = document.getElementById("credit");
 footer.innerHTML = doi;
 footer.href = "https://www.instagram.com/itspapoy/";
